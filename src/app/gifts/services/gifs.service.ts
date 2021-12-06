aa12dbf9037c4d4eda9eb474d159eb6a4b8e8291
david <david_drcr@hotmail.com>
2021-12-06 17:29:24 -0500

    7-15. LocalStorage, 16. Cargar imágenes automáti

--- src/app/gifts/services/gifs.service.ts
+++ src/app/gifts/services/gifs.service.ts
@@ -6,13 +6,18 @@ import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
   providedIn: 'root',
 })
 export class GifsService {
+  URL_GIPHY = 'https://api.giphy.com/v1/gifs/';
+  LIMIT = 10;
   private apiKey: string = '';
   private _history: string[] = [];
 
   // TODO: change any for apropiated type
   public resultados: Gif[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this._history = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+  }
 
   get history(): string[] {
     return [...this._history];
@@ -24,15 +29,18 @@ export class GifsService {
     if (!this._history.includes(query)) {
       this._history.unshift(query);
       this._history = this._history.splice(0, 10);
+
+      localStorage.setItem('historial', JSON.stringify(this._history));
     }
 
     this.http
       .get<SearchGifsResponse>(
-        `https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&limit=7&q=${query}`
+        `${this.URL_GIPHY}search?api_key=${this.apiKey}&limit=${this.LIMIT}&q=${query}`
       )
       .subscribe((resp) => {
         console.log(resp.data);
         this.resultados = resp.data;
+        localStorage.setItem('resultados', JSON.stringify(this.resultados));
       });
   }
 }
