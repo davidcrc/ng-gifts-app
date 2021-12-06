79b546644dce24bdb59a7c5a0374ef0202fefe5e
david <david_drcr@hotmail.com>
2021-12-06 17:36:17 -0500

    7-17. Obtener imágenes desde el sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -12,4 +12,9 @@ export class SidebarComponent {
   }
 
   constructor(private gifsService: GifsService) {}
+
+  buscar(termino: string) {
+    // console.log('termino', termino);
+    this.gifsService.buscarGifs(termino);
+  }
 }
